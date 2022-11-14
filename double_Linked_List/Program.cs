f8f5584c31032784c131d20bcacc5950508e109a
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-14 17:20:43 +0700

    add method search

--- double_Linked_List/Program.cs
+++ double_Linked_List/Program.cs
@@ -52,9 +52,46 @@ namespace double_linked_list
                 newNode.next = START;
                 if(START != null)
                     START.prev = newNode;
-
+                newNode.prev = null;
+                START = newNode;
+                return;
             }
+            //if the node is to be inserted at between two node*/
+            Node previous, current;
+            for (current= previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return ;
+                }
+            }
+            /* On the execution of the above for loop, prev and
+             * * current will point to those nodes
+             * * between wich the new node is to be inserted */
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be insarted at the end list
+            if (current == null)
+            {
+                newNode.next = null;
+                newNode.prev = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
         }
+        public bool search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null && 
+                rollNo != current.noMhs; previous = current,
+                current = current.next) { }
+            return (current != null);
+        }
+        
     }
 
     internal class Program
