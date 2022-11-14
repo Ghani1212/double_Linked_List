0dc5263e946e7635bc547b65d4e06c1519250bf8
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-14 16:52:42 +0700

    add class node

--- double_Linked_List/Program.cs
+++ double_Linked_List/Program.cs
@@ -4,8 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace double_Linked_List
+namespace double_linked_list
 {
+    class Node
+    {
+        /* Node class represents the node of doubly linked list.
+         * it consist of the information part and links to.
+         * its seccending and preeceding
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the suceeding node
+        public Node next;
+        //point to the preeceding node
+        public Node prev;
+    }
     internal class Program
     {
         static void Main(string[] args)
