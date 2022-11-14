00dd8c3cd2dbbf59ad907fbd9cb5fb24624a94d8
Ghani Zumar <ghani.zumar.ft21@mail.umy.ac.id>
2022-11-14 17:03:32 +0700

    membuat check list empty

--- double_Linked_List/Program.cs
+++ double_Linked_List/Program.cs
@@ -19,6 +19,44 @@ namespace double_linked_list
         //point to the preeceding node
         public Node prev;
     }
+
+    class DoubleLinkedList
+    {
+        Node START;
+
+        //constructor
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the strudent: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if(START != null)
+                    START.prev = newNode;
+
+            }
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
